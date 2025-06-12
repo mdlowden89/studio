e651fb0953e2ab517a83b651731c2eb5c6412a38
Matt Lowden <mlowdencrossd@gmail.com>
2025-06-12 16:05:41 +0000

    change blur to 5px

--- src/components/discover/blurred-likes-section.tsx
+++ src/components/discover/blurred-likes-section.tsx
@@ -40,7 +40,7 @@ export function BlurredLikesSection() {
             style={{
               transform: `rotate(${index * 5 - (mockLikers.length > 1 ? (mockLikers.length -1) * 2.5 : 0)}deg) translateX(${index * 12 - (mockLikers.length > 1 ? (mockLikers.length -1) * 6 : 0)}px) translateY(${index * -6}px)`,
               zIndex: mockLikers.length - index,
-              filter: 'blur(4px)', 
+              filter: 'blur(5px)', 
             }}
           >
             <Image
